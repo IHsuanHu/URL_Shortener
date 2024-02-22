e5f8384c3627f0bd8726c5aa0a7acc446198fb68
IHsuanHu <ihsuanhu@gmail.com>
2024-02-22 17:14:05 -0500

    finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+
 import { Component } from '@angular/core';
+import { NgTinyUrlService } from 'ng-tiny-url';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,68 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UrlShortener';
+  model = {
+    inputUrl:''
+  }
+  isFormSubmitted = false;
+  isLoading = false;
+  shortUrl = '';
+  isTextCopied = false;
+  constructor(private tinyUrl:NgTinyUrlService) {}
+
+  onSubmitUrlForm(urlForm:any) {
+    if(urlForm.valid) {
+      this.isLoading = true;
+      this.tinyUrl.shorten(this.model.inputUrl).subscribe((data) =>{
+        this.shortUrl = data;
+        this.isFormSubmitted = true;
+        this.isLoading = false;
+      } ,(error) =>{
+        alert("Wrong URL input");
+        this.isLoading = false;
+      })
+    }
+  }
+  reset() {
+    this.model.inputUrl = '';
+    this.isFormSubmitted = false;
+    this.isTextCopied = false;
+  }
+  
+  copy(shortUrlElementRef:any) {
+    let InputElement = document.createElement('input')
+    InputElement.setAttribute('type', 'text')
+    InputElement.setAttribute('value', shortUrlElementRef.innerHTML)
+    InputElement.select();
+    InputElement.setSelectionRange(0, 999999);
+    try{
+      navigator.clipboard.writeText(InputElement.value);
+      this.isTextCopied = true;
+      setTimeout(() => {
+        this.isTextCopied = false;
+      },2000);
+    } catch(e:any) {
+      console.warn('Error while coping..', e.toString())
+    }
+  }
+
+  // shortenedUrl = '';
+  // url = '';
+
+  // constructor(public tinyUrl: NgTinyUrlService) {
+  //   // this.tinyUrl.shorten(this.url).subscribe(res => {
+  //   //   this.shortenedUrl = res;
+  //   // })
+  // }
+  
+  // generateUrl(value:string){
+  //   this.url = value;
+  //   this.tinyUrl.shorten(this.url).subscribe(res => {
+  //     this.shortenedUrl = res;
+  //   })
+    
+  // }
+  // resetUrl() {
+  //   this.shortenedUrl = '';
+  // }
 }
